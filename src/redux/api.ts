8cd3a6b27953e0bb866d04e87044cabf93d4f7ee
Aniket Singh <aniketsingh90126@gmail.com>
2024-12-29 20:32:03 +0530

    feat: rtk query with redux done

--- src/redux/api.ts
+++ src/redux/api.ts
@@ -1,7 +1,7 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const myApi = createApi({
-  reducerPath: "api",
+  reducerPath: "myapi",
   baseQuery: fetchBaseQuery({
     baseUrl: "http://localhost:3000",
   }),
